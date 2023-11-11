f38f55358e0fd7975c31398f90f4d4aed645ad9a
jun <ggprgrkjh@naver.com>
2023-11-12 00:08:14 +0900

    docs: 구현할 기능 목록 수정

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,4 +1,12 @@
 package christmas.controller;
 
+import christmas.model.Calculator;
+import christmas.model.Menu.Menu;
+import christmas.view.InputView;
+
 public class ChristmasController {
+    public void start() {
+
+    }
+
 }
