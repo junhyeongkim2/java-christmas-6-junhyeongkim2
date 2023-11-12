9e68bbb1a9105795b3f51b50c7f88afa7ad38894
jun <ggprgrkjh@naver.com>
2023-11-13 01:19:06 +0900

    feat(christmascontroller): 예외 입력 발생시 재입력 기능 추가

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -10,23 +10,28 @@ import christmas.view.OutputView;
 public class ChristmasController {
 
     private Planner planner;
+    int day;
+    Menus menus;
 
     public ChristmasController(Planner planner) {
         this.planner = planner;
     }
 
     public void start() {
-        int day = Integer.parseInt(InputView.readVisitDay());
-        Menus menus = planner.isContainMenu(InputView.readMenus());
+        try {
+            day = Integer.parseInt(InputView.readVisitDay());
+            menus = planner.isContainMenu(InputView.readMenus());
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
         OutputView.printStartBenefitMessage(day);
         OutputView.printMenus(menus.getMenus());
         OutputView.printTotalOrderAmount(planner.requestTotalOrderAmount(menus));
-        OutputView.printGiveawayMenu(planner.requestGiveawayMenuResult(menus,day));
+        OutputView.printGiveawayMenu(planner.requestGiveawayMenuResult(menus, day));
         OutputView.printEventResult(planner.requestEventResult(menus, day).getEventResult());
         OutputView.printTotalBenefitAmount(planner.requestTotalBenefitAmount(menus, day));
         OutputView.printExpectedPaymentAmount(planner.requestExpectedPaymentAmount());
         OutputView.printBadge(planner.requestBadge());
-
     }
 
 }
