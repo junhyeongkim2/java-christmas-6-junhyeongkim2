a5b1fc8f81ed8b25c8dcc6271e13930204d0800a
jun <ggprgrkjh@naver.com>
2023-11-13 18:07:39 +0900

    refactor(calculator): CalculatorTest 코드 Before 이용하여 리팩토링

--- src/test/java/christmas/domain/CalculatorTest.java
+++ src/test/java/christmas/domain/CalculatorTest.java
@@ -6,6 +6,7 @@ import christmas.model.Menu.Menus;
 import christmas.model.Planner;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,13 +15,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+    private Planner planner;
+
+    private Menus menus;
+
+    @BeforeEach
+    void setUp() {
+        this.calculator = new Calculator();
+        this.planner = new Planner();
+    }
+
     @DisplayName("총 주문 금액 계산 성공 테스트")
     @Test
     void calculateTotalOrderAmount_EqualResult_Success() {
         //given
-        Calculator calculator = new Calculator();
-        Planner planner = new Planner();
-        Menus menus = planner.isContainMenu(
+        menus = planner.isContainMenu(
                 InputView.splitMenuAndCount(InputView.splitMenuWithComma("초코케이크-1,바비큐립-1,샴페인-1")));
         //when
         long totalOrderAmount = calculator.calculateTotalOrderAmount(menus);
@@ -33,9 +43,7 @@ public class CalculatorTest {
     @Test
     void calculateTotalOrderAmount_DifferentResult_Fail() {
         //given
-        Calculator calculator = new Calculator();
-        Planner planner = new Planner();
-        Menus menus = planner.isContainMenu(
+        menus = planner.isContainMenu(
                 InputView.splitMenuAndCount(InputView.splitMenuWithComma("초코케이크-5,바비큐립-1,샴페인-1")));
         //when
         long totalOrderAmount = calculator.calculateTotalOrderAmount(menus);
@@ -50,9 +58,7 @@ public class CalculatorTest {
     @Test
     void calculateTotalBenefitAmount_WeekdayEqualResult_Success() {
         //given
-        Calculator calculator = new Calculator();
-        Planner planner = new Planner();
-        Menus menus = planner.isContainMenu(
+        menus = planner.isContainMenu(
                 InputView.splitMenuAndCount(InputView.splitMenuWithComma("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1")));
         EventChecker eventChecker = new EventChecker();
         eventChecker.createEventResult(menus, 3);
@@ -70,9 +76,7 @@ public class CalculatorTest {
     @Test
     void calculateTotalBenefitAmount_WeekendEqualResult_Success() {
         //given
-        Calculator calculator = new Calculator();
-        Planner planner = new Planner();
-        Menus menus = planner.isContainMenu(
+        menus = planner.isContainMenu(
                 InputView.splitMenuAndCount(InputView.splitMenuWithComma("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1")));
 
         //when
@@ -80,7 +84,6 @@ public class CalculatorTest {
 
         //then
         assertThat(result).isEqualTo(31546);
-
 
     }
 
