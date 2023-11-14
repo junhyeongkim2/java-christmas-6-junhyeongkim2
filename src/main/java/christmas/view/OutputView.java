efadb93ffa9eb80274fbc3b1d4ae10665d264937
jun <ggprgrkjh@naver.com>
2023-11-14 19:26:16 +0900

    refactor(outputview): 메세지 상수 처리

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -9,64 +9,80 @@ import java.util.Map;
 
 public class OutputView {
     private static final DecimalFormat df = new DecimalFormat("###,###");
+    private static final String START_PLANNER_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String DECEMBER = "12월";
+    private static final String START_BENEFIT_MESSAGE = "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    private static final String ORDER_MENU = "<주문 메뉴>";
+    private static final String COUNT = "개";
+    private static final String SPACE = " ";
+    private static final String TOTAL_ORDER_AMOUNT_MESSAGE = "<할인 전 총주문 금액>";
+    private static final String WON = "원";
+    private static final String GIVEAWAY_MESSAGE = "<증정 메뉴>";
+    private static final String CHAMPAGNE_MESSAGE = "샴페인 1개";
+    private static final String NONE_MESSAGE = "없음";
+    private static final String BENEFIT_LIST_MESSAGE = "<혜택 내역>";
+    private static final String TOTAL_BENEFIT_MESSAGE = "<총혜택 금액>";
+    private static final String EXPECTED_PAYMENT_MESSAGE = "<할인 후 예상 결제 금액>";
+    private static final String BADGE_MESSAGE = "<12월 이벤트 배지>";
+
 
     public static void printStartChristmasPlannerMessage() {
-        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println(START_PLANNER_MESSAGE);
     }
 
     public static void printStartBenefitMessage(int day) {
-        System.out.println("12월" + " " + day + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
+        System.out.println(DECEMBER + " " + day + START_BENEFIT_MESSAGE);
     }
 
     public static void printMenus(Map<Menu, Integer> menus) {
-        System.out.println("<주문 메뉴>");
+        System.out.println(ORDER_MENU);
         menus.keySet().stream().filter(key -> menus.get(key) != 0).forEach(
                 key -> System.out.println(
-                        key + " " + menus.get(key) / Menu.valueOf(String.valueOf(key)).getPrice() + "개")
+                        key + SPACE + menus.get(key) / Menu.valueOf(String.valueOf(key)).getPrice() + COUNT)
         );
         System.out.println();
     }
 
     public static void printTotalOrderAmount(long totalOrderAmount) {
-        System.out.println("<할인 전 총주문 금액>");
-        System.out.println(replaceToDecimalFormat(totalOrderAmount) + "원\n");
+        System.out.println(TOTAL_ORDER_AMOUNT_MESSAGE);
+        System.out.println(replaceToDecimalFormat(totalOrderAmount) + WON + "\n");
     }
 
 
     public static void printGiveawayMenu(DiscountInfo giveawayMenu) {
-        System.out.println("<증정 메뉴>");
+        System.out.println(GIVEAWAY_MESSAGE);
         if (-giveawayMenu.getDiscount() == Menu.샴페인.getPrice()) {
-            System.out.println("샴페인 1개\n");
+            System.out.println(CHAMPAGNE_MESSAGE + "\n");
         }
         if (giveawayMenu.getDiscount() == 0) {
-            System.out.println("없음\n");
+            System.out.println(NONE_MESSAGE + "\n");
         }
     }
 
     public static void printEventResult(List<DiscountInfo> eventResult) {
-        System.out.println("<혜택 내역>");
+        System.out.println(BENEFIT_LIST_MESSAGE);
         if (eventResult.size() == 0) {
-            System.out.println("없음\n");
+            System.out.println(NONE_MESSAGE + "\n");
             return;
         }
         eventResult.stream()
                 .forEach(result -> System.out.println(
-                        result.getName() + ": " + replaceToDecimalFormat(result.getDiscount()) + "원"));
+                        result.getName() + ": " + replaceToDecimalFormat(result.getDiscount()) + WON));
         System.out.println();
     }
 
     public static void printTotalBenefitAmount(long totalBenefitAmount) {
-        System.out.println("<총혜택 금액>");
-        System.out.println(replaceToDecimalFormat(+totalBenefitAmount) + "원\n");
+        System.out.println(TOTAL_BENEFIT_MESSAGE);
+        System.out.println(replaceToDecimalFormat(+totalBenefitAmount) + WON + "\n");
     }
 
     public static void printExpectedPaymentAmount(long expectedPaymentAmount) {
-        System.out.println("<할인 후 예상 결제 금액>");
-        System.out.println(replaceToDecimalFormat(expectedPaymentAmount) + "원\n");
+        System.out.println(EXPECTED_PAYMENT_MESSAGE);
+        System.out.println(replaceToDecimalFormat(expectedPaymentAmount) + WON + "\n");
     }
 
     public static void printBadge(Badge badge) {
-        System.out.println("<12월 이벤트 배지>");
+        System.out.println(BADGE_MESSAGE);
         System.out.println(badge);
     }
 
