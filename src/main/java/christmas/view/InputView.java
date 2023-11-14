5a3a3fdafe5e6af04abeaa7ae0a389cbaaddb38c
jun <ggprgrkjh@naver.com>
2023-11-14 18:58:25 +0900

    refactor(inputview): 메세지 상수 처리 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -12,9 +12,21 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final String MENU_PATTERN = "([\\w\\S]+-\\d+,)*[\\w\\S]+-\\d+";
+    private static final String READ_VISITDAY_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String READ_MENUS_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static final String INVALID_ORDER_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String INVALID_VISITDAY_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String DRINK_CATEGORY = "음료";
+    private static final int MENU = 0;
+    private static final int COUNT = 1;
+    private static final int MAX_MENU_COUNT = 20;
+    private static final int VALID_SPLIT = 2;
+
+    private static final int VISITDAY_START = 1;
+    private static final int VISITDAY_END = 31;
 
     public static String readVisitDay() {
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        System.out.println(READ_VISITDAY_MESSAGE);
         return repeatReadVisitDay();
     }
 
@@ -31,7 +43,7 @@ public class InputView {
     }
 
     public static Map<Menu, Integer> readMenus() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        System.out.println(READ_MENUS_MESSAGE);
         return repeatReadMenus();
     }
 
@@ -65,56 +77,56 @@ public class InputView {
         for (String splitMenu : splitMenuWithComma) {
             String[] menuAndCount = splitMenuWithHypen(splitMenu);
             validateMenuAndCount(splitMenuWithHypen(splitMenu), orderedMenu);
-            orderedMenu.putIfAbsent(Menu.valueOf(menuAndCount[0]),
-                    Integer.parseInt(menuAndCount[1]) * Menu.valueOf(menuAndCount[0]).getPrice());
+            orderedMenu.putIfAbsent(Menu.valueOf(menuAndCount[MENU]),
+                    Integer.parseInt(menuAndCount[COUNT]) * Menu.valueOf(menuAndCount[MENU]).getPrice());
         }
     }
 
     public static void validateIsUnderTwentyMenu(Map<Menu, Integer> orderedMenu) {
         int menuCount = orderedMenu.keySet().stream()
                 .mapToInt(key -> orderedMenu.get(key) / Menu.valueOf(String.valueOf(key)).getPrice()).sum();
-        if (menuCount > 20) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        if (menuCount > MAX_MENU_COUNT) {
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
         }
     }
 
     public static void validateMenuAndCount(String[] menuAndCount, Map<Menu, Integer> orderedMenu) {
         validateSplit(menuAndCount);
-        validateIsIntegerMenuCount(menuAndCount[1]);
-        validateIsContainMenu(menuAndCount[0]);
-        validateIsOverOneMenu(Integer.parseInt(menuAndCount[1]));
-        validateDuplicatedMenu(orderedMenu, Menu.valueOf(menuAndCount[0]));
+        validateIsIntegerMenuCount(menuAndCount[COUNT]);
+        validateIsContainMenu(menuAndCount[MENU]);
+        validateIsOverOneMenu(Integer.parseInt(menuAndCount[COUNT]));
+        validateDuplicatedMenu(orderedMenu, Menu.valueOf(menuAndCount[MENU]));
     }
 
-    public static void validateSplit(String[] menuAndcount) {
-        if (menuAndcount.length != 2) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    public static void validateSplit(String[] menuAndCount) {
+        if (menuAndCount.length != VALID_SPLIT) {
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
         }
 
     }
 
     public static void validateIsDrinkOnlyMenu(Set<Menu> keySet) {
-        if (keySet.stream().filter(key -> key.getCategory().equals("음료")).count() == keySet.size()) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        if (keySet.stream().filter(key -> key.getCategory().equals(DRINK_CATEGORY)).count() == keySet.size()) {
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
         }
     }
 
     public static void validateDuplicatedMenu(Map<Menu, Integer> orderedMenu, Menu menu) {
         if (orderedMenu.get(menu) != null) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
         }
     }
 
     public static void validateIsContainMenu(String menu) {
         Boolean find = Arrays.stream(Menu.values()).map(Menu::getName).collect(Collectors.toList()).contains(menu);
         if (find == false) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
         }
     }
 
     public static void validateIsOverOneMenu(int count) {
         if (count < 1) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
         }
     }
 
@@ -122,13 +134,13 @@ public class InputView {
         try {
             Integer.parseInt(count);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
         }
     }
 
     public static String validateMenuForm(String input) {
         if (!Pattern.matches(MENU_PATTERN, input)) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
         }
         return input;
     }
@@ -136,8 +148,8 @@ public class InputView {
 
     public static void validateInRange(String input) {
         int day = Integer.parseInt(input);
-        if (day < 1 || day > 31) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        if (day < VISITDAY_START || day > VISITDAY_END) {
+            throw new IllegalArgumentException(INVALID_VISITDAY_MESSAGE);
         }
     }
 
@@ -145,7 +157,7 @@ public class InputView {
         try {
             Integer.parseInt(input);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(INVALID_VISITDAY_MESSAGE);
         }
     }
 
