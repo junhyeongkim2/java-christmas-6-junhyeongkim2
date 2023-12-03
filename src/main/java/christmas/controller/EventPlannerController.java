4366570318867d6aec0180ed541813d4f3fb526c
jun <ggprgrkjh@naver.com>
2023-12-04 02:05:34 +0900

    refactoor(eventplannercontroller): 컨트롤러 기능 분리

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -10,17 +10,27 @@ public class EventPlannerController {
     public void start() {
         OutputView.printStartMessage();
         int visitDay = repeatUntilValid(() -> Integer.parseInt(InputView.readVisitDay()));
-        Order order = repeatUntilValid(() -> Order.of(InputView.readMenus()));
-        OutputView.printBenefitStartMessage(visitDay);
-        OutputView.printMenus(order.toString());
-        OutputView.printTotalOrderAmount(order.calculateTotalOrderAmount());
-        OutputView.printIsGiveaway(order.isGiveaway());
+        Order order = printOrderInfo(visitDay);
+        printEventResult(visitDay, order);
+    }
+
+    private void printEventResult(int visitDay, Order order) {
         EventResult eventResult = EventResult.from(visitDay, order);
         OutputView.printWinningEvents(eventResult);
         OutputView.printTotalBenefitAmount(eventResult.calculateTotalBenefit());
         OutputView.printExpectedPaymentAmount(eventResult.calcualteExpectedPaymentAmount());
         OutputView.printBadge(eventResult.calculateBadge());
     }
+
+    private Order printOrderInfo(int visitDay) {
+        Order order = repeatUntilValid(() -> Order.of(InputView.readMenus()));
+        OutputView.printBenefitStartMessage(visitDay);
+        OutputView.printMenus(order.toString());
+        OutputView.printTotalOrderAmount(order.calculateTotalOrderAmount());
+        OutputView.printIsGiveaway(order.isGiveaway());
+        return order;
+    }
+
 
     private <T> T repeatUntilValid(Supplier<T> function) {
         try {
