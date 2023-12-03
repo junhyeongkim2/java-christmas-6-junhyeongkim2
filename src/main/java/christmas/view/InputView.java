fa4dea1aa6f2da42af8cceb8d1883fd57f8ef542
jun <ggprgrkjh@naver.com>
2023-12-03 21:01:51 +0900

    feat(inputview): 방문날짜 숫자 검증 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     public static String readVisitDay() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
+        validateIsNumber(input);
         return input;
     }
 
@@ -14,6 +15,14 @@ public class InputView {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
         return input;
+    }
+
+    public static void validateIsNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 
 
