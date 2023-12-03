44d1ae3534c178340df5a88b2112995e989d0ef8
jun <ggprgrkjh@naver.com>
2023-12-03 18:23:31 +0900

    feat(outputview): 이벤트 혜택 미리보기 시작 멘트 출력 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -6,4 +6,8 @@ public class OutputView {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
 
+    public static void printBenefitStartMessage(int visitDay) {
+        System.out.println("12월 " + visitDay + "에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+
 }
